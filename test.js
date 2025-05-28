a2671859f9c1be3c768f33eb812cd276136b7e54
Ian Renauld <ian@qaclique.com>
2025-05-28 10:17:35 -0400

    Use import, force Karate version and handle "clean" error when target folder is missing

--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 #! /usr/bin/env node
-const karate = require("@karatelabs/karate");
+import karate from "@karatelabs/karate";
+import fs from "node:fs";
+
+karate.version = "1.5.1";
+
+// Create the target directory if it doesn't exist
+const targetDir = "./target";
+if (!fs.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir);
+}
+
 // Options:
 // -T=10: Set the number of threads to 10
-// -C: Clean (delete) the target directory before running - this will fail on first run since target directory doesn't exist
+// -C: Clean (delete) the target directory before running
 // -f junit:xml: Specify the report output format as JUnit XML (still does Karate HTML report as well)
-// karate.exec("-T=10 -C -f junit:xml");
-karate.exec("-T=10 -f junit:xml");
+karate.exec("-T=10 -C -f junit:xml");
